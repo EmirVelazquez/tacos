af92b15d365f02984997cb0ef4cee13128c397c2
EmirVelazquez <emirleovelazquez@gmail.com>
2019-12-16 22:03:22 -0600

    preparing app for deployment to heroku, added JAWSDB in connection.js for heroku mysql database

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,17 @@ var keys = require("../keys.js");
 var mysql = require("mysql");
 
 // Creating the connection here, password has to be changed or use a dotenv
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: keys.server.password, // Must create a .env file with password to MySQL
-    database: "tacos_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: keys.server.password, // Must create a .env file with password to MySQL
+        database: "tacos_db"
+    });
+};
 
 // Making the connection here
 connection.connect(function (err) {
